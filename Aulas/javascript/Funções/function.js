f7a7c1e3c3430392af9ab1fe00cd6c9edcbe9449
Kevin Dias <kevindevdbs@gmail.com>
2024-11-05 18:43:11 -0300

    att

---
+++ Aulas/javascript/Funções/function.js
@@ -0,0 +1,40 @@
+// function funcao(num1,num2) {
+//     console.log('A soma entre ' + num1 + ' e ' + num2 + ' eh ' + (num1 + num2));
+// }
+
+// funcao(10,20);
+
+
+
+
+
+// var variavel = function (){
+//     console.log('Olá mundo');
+//     console.log = (typeof(variavel));
+// }
+
+// variavel();
+
+
+
+
+
+// var obj = { nome:'João', idade: 25, cidade: 'São Paulo'};
+
+// obj.funcao = function (){
+//     alert(`O nome e ${obj.nome} a idade e ${obj.idade} a cidade e ${obj.cidade}`);
+// }
+
+// obj.funcao();
+
+
+
+
+
+var variavel = [1,2,3,4,5,6,7,8,9,10];
+variavel [0] = ['joao', 'maria', 'pedro', 'joaquim'];
+variavel[1] = [8,7,6,5,4,3,2,1];
+
+console.log(variavel);
+
+    
