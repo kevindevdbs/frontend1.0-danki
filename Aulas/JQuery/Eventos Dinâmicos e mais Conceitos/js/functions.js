549d833f6fe74b1c4bbfeff78ae0ab0cf88bd7b4
Kevin Dias <kevindevdbs@gmail.com>
2024-11-11 12:20:35 -0300

    att

---
+++ Aulas/JQuery/Eventos Dinâmicos e mais Conceitos/js/functions.js
@@ -0,0 +1,21 @@
+$(function () {
+/*
+var link = $('<a href="#">Link</a>').appendTo('body');
+var div = $('<div></div>').appendTo('body').hide();
+
+$('div').css({
+    'background-color':'red',
+    'width':'100px',
+    'height':'100px'
+});
+
+link.on('click', function () {
+   div.toggle();
+   alert('Cliquei');
+});
+*/
+
+$('input[name="nome-pessoa"]').on('keyup', function () {
+    
+});
+});
