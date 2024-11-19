8319eba2764678daf31a3f23966f252eedff4ccb
Kevin Dias <kevindevdbs@gmail.com>
2024-11-18 21:05:50 -0300

    Atualização

---
+++ Projetos/Projeto 3/js/functions.js
@@ -0,0 +1,37 @@
+$(function () {
+
+    abrirmenu();
+    scrollmenu();
+
+    function abrirmenu(){
+
+        $('nav.mobile-menu').on('click', function () {
+            $(this).find('ul').slideToggle();
+        });
+    }
+
+    $(function scrollmenu () {
+    
+    // Função para fazer scroll suave para as seções
+    $('nav a').on('click', function (event) {
+        event.preventDefault(); // Evita o comportamento padrão do link
+        
+        var target = $(this).attr('href'); // Obtém o valor do atributo href do link
+        
+        // Calcula a posição do scroll para centralizar a seção na tela
+        var targetOffset = $(target).offset().top;
+        var windowHeight = $(window).height();
+        var sectionHeight = $(target).outerHeight();
+        var scrollTo = targetOffset - (windowHeight / 2) + (sectionHeight / 2);
+        
+        // Faz scroll suave até a seção correspondente
+        $('html, body').animate({
+            scrollTop: scrollTo
+        }, 1000); // Duração do scroll em milissegundos
+    });
+});
+
+});
+
+
+
